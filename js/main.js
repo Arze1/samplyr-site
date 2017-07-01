0f186531cec5746d503e26de7c49ae9ef6544bfc
Arze1 <Arze1@users.noreply.github.com>
2017-07-01 11:30:01 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $("document").ready(function() {
 	$("#typetype").typed({
-		strings: ["string", "object", "array","email","phone", "any"],
+		strings: ["object", "array","any"],
 		typeSpeed: 100,
 		loop: true,
 		loopCount: Infinity
 	});
 	var dt = new Date();
 
-	fetch(`https://api.npmjs.org/downloads/point/2017-06-29:${dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate()}/verifyr`).then(function(response) { 
+	fetch(`https://api.npmjs.org/downloads/point/2017-06-29:${dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate()}/samplyr`).then(function(response) { 
 		return response.json();
 	}).then(function(j) {
 		$(".downloads").html(j.downloads || "(ERROR)"); 
